2b1064aa36a1de33d2a4815d2baeecb918941301
Woochan <xodlqodj12@naver.com>
2023-04-07 16:53:44 +0900

    경로변경

--- setting.py
+++ setting.py
@@ -1,7 +1,7 @@
 import os 
 
-file_path = 'C:/Users/user/Desktop/2기_Woochan/deep_learning_basic/dataset/laptops.csv'
-output_path = 'C:/Users/user/Desktop/2기_Woochan/deep_learning_basic/dataset/output/laptops_output.csv'
+file_path = 'C:/Users/user/Desktop/2기_Woochan/git/DeepFM/dataset/laptops.csv'
+output_path = 'C:/Users/user/Desktop/2기_Woochan/git/DeepFM/dataset/output/laptops_output.csv'
 drop_column = ['img_link','no_of_ratings','no_of_reviews']
 sparse_column = ['name', 'processor', 'ram', 'os', 'storage']
 dense_column = ['price(in Rs.)','display(in inch)']
