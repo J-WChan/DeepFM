2ba7a00d0ffe7e8118d392a1d77ada4cddf15fd9
J-WChan <129950810+J-WChan@users.noreply.github.com>
2023-04-12 17:39:18 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,7 +1,7 @@
 import os 
 
-file_path = 'C:/Users/user/Desktop/2기_Woochan/git/DeepFM/dataset/laptops.csv'
-output_path = 'C:/Users/user/Desktop/2기_Woochan/git/DeepFM/dataset/output/laptops_output.csv'
+file_path = './dataset/laptops.csv'
+output_path = './output/laptops_output.csv'
 drop_column = ['img_link','no_of_ratings','no_of_reviews']
 sparse_column = ['name', 'processor', 'ram', 'os', 'storage']
 dense_column = ['price(in Rs.)','display(in inch)']
